b49ceeae2486d0ae99942e50f677741aa1027325
JasonMarquez8 <88029339+JasonMarquez8@users.noreply.github.com>
2022-06-13 19:24:15 +1000

    Add files via upload

---
+++ My Personal Portfolio/js/form1.js
@@ -0,0 +1,14 @@
+ // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optionals
+var firebaseConfig = {
+    apiKey: "AIzaSyA1yM3SPvwb8J8UHMcjyD1v2y5zaUkPO7Q",
+    authDomain: "my-personal-portfolio-8f8f3.firebaseapp.com",
+    databaseURL: "https://my-personal-portfolio-8f8f3-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "my-personal-portfolio-8f8f3",
+    storageBucket: "my-personal-portfolio-8f8f3.appspot.com",
+    messagingSenderId: "45171002576",
+    appId: "1:45171002576:web:bc6371254be18deed6a2d5",
+    measurementId: "G-KM97B4FS8Q"
+};
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
